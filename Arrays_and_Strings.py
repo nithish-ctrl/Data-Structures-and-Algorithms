feb5eaeadd1dd47f72e8266c0d3a7a6e15f7c1d0
Nithish <nithish221b@gmail.com>
2026-05-23 21:38:48 +0530

    Update commit

---
+++ Arrays_and_Strings.py
@@ -0,0 +1,10 @@
+'''
+Arrays : 
+1. Static Arrays
+2. Dynamic Arrays - Lists in Python
+    Dynamic Arrays are made up of static arrays
+
+
+String - Immutable or Hashable
+Almost eveything is O(n) except for random acess and length checking
+'''
