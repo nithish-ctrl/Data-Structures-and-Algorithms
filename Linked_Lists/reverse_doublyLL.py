86134296aca14855313b45791303886d85f1aae9
Nithish <nithish221b@gmail.com>
2026-05-27 01:28:10 +0530

    Reverse a DLL

--- Linked_Lists/reverse_doublyLL.py
+++ Linked_Lists/reverse_doublyLL.py
@@ -1,12 +1,16 @@
 from Linked_lists import Doubly_Linkedlist
 
 def Reverse_DLL(head):
+    temp = None
     curr = head 
-    while curr : 
+    while curr: 
         temp = curr.prev
         curr.prev = curr.next
         curr.next = temp
         curr = curr.prev
+    if temp is not None :
+        head = temp.prev
+    return head
     
 
 head = Doubly_Linkedlist(3)
@@ -15,5 +19,6 @@ b=Doubly_Linkedlist(5,a)
 head.next=a
 a.next = b
 head.display_Linkedlists(head)
-Reverse_DLL(head)
+head = Reverse_DLL(head)
 head.display_Linkedlists(head)
+
