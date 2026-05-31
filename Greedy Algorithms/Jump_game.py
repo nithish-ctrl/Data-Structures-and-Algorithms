ea632c26f58db7abc7ffcce80677cc4e2217c2d6
Nithish <nithish221b@gmail.com>
2026-05-31 13:32:10 +0530

    Jump Game

---
+++ Greedy Algorithms/Jump_game.py
@@ -0,0 +1,28 @@
+
+def jump_game(arr):
+    target_index = len(arr)-1
+    curr_index = 0
+    max_index = 0
+    for i,idx in enumerate(arr):
+        if i + idx > target_index and i == len(arr)-1:
+            return False
+        elif i+idx == target_index:
+            return True
+    return False
+
+'''
+nums = [2, 3, 1, 0, 4]
+print(jump_game(nums))
+
+nums = [3, 2, 1, 0, 4]
+print(jump_game(nums))
+
+nums = [1,2]
+print(jump_game(nums))
+
+nums = [3,2]
+print(jump_game(nums))
+
+nums = [1,3,2,4,5]
+print(jump_game(nums))
+'''
