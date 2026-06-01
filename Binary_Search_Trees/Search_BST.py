cd177501be005594c52072190aa058b8c8d57d2f
Nithish <nithish221b@gmail.com>
2026-06-01 20:29:31 +0530

    Search in a Binary Search Tree

---
+++ Binary_Search_Trees/Search_BST.py
@@ -0,0 +1,36 @@
+from Binary_tree import TreeNode
+
+def Search_BST(node,target):
+    while node: 
+        if node.val == target : return f'The target is found at {node}'
+        elif node.val < target : return Search_BST(node.right, target)
+        else :
+            return Search_BST(node.left, target)
+    return False
+
+'''
+a = TreeNode(8)
+b = TreeNode(5)
+c = TreeNode(12)
+d = TreeNode(4)
+e = TreeNode(7)
+f = TreeNode(10)
+g = TreeNode(14)
+h = TreeNode(6)
+i = TreeNode(13)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.left = f
+c.right = g
+e.left = h
+g.left = i
+
+a.Preorder_traversal(a)
+print('\r')
+print(Search_BST(a, 6))
+'''
+
+
