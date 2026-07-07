9f80c1f78d66033d99ccbca99384bed7e12d40a3
Nithish <nithish221b@gmail.com>
2026-07-07 20:35:23 +0530

    Replacing only once

--- Arrays/Product_Array.py
+++ Arrays/Product_Array.py
@@ -4,13 +4,13 @@ def product_array(nums : list[int]) -> list[int]:
     string_list = '*'.join(map(str,nums))
     Product_list = []
     for i in nums : 
-        product = eval(string_list.replace(str(i), '1'))
+        product = eval(string_list.replace(str(i), '1', 1))
         Product_list.append(product)
     return Product_list
 
 
 
-nums = [1,2,3,4]
+nums = [1,2,3,4,2]
 Product_list = product_array(nums=nums)
 print(f'The input list is : {nums}')
 print(f'The product of the array except itself is : {Product_list}')
