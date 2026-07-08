4f2a0db7ed770756a737b0b2e1456bb133b17c83
Nithish <nithish221b@gmail.com>
2026-07-08 11:38:24 +0530

    Longest Consecutive Sequence

---
+++ Arrays/Longest_Consecutive_Seq.py
@@ -0,0 +1,19 @@
+
+def Longest_Consecutive_Seq(nums : list[int]) -> int:
+    if not nums : return 0
+
+    num_set = set(nums)
+    longest_streak = 0
+
+    for num in num_set : 
+        if num - 1 not in num_set : 
+            longest = 1
+            while num+longest in num_set : 
+                longest += 1
+        longest_streak = max(longest_streak, longest)
+    return longest_streak
+
+number_list = [100,33,45,65,12,13,14,15]
+Longest = Longest_Consecutive_Seq(nums = number_list)
+print(f'The input list is : {number_list}')
+print(f'The length of the longest consecutive sequence is : {Longest}')
