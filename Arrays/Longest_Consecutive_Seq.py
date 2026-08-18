38b2c19cf3ac54abcaf9550d5df3315a5014ccde
Nithish <nithish221b@gmail.com>
2026-08-18 21:52:21 +0530

    Adding comments and revising

--- Arrays/Longest_Consecutive_Seq.py
+++ Arrays/Longest_Consecutive_Seq.py
@@ -1,14 +1,14 @@
-
+# Find the length of the longest consecutive sequence in a unsorted array of integers. 
 def Longest_Consecutive_Seq(nums : list[int]) -> int:
     if not nums : return 0
 
     num_set = set(nums)
-    longest_streak = 0
+    longest_streak = 0  # Final result to return
 
     for num in num_set : 
         if num - 1 not in num_set : 
             longest = 1
-            while num+longest in num_set : 
+            while num+longest in num_set : # Loop till every consecutive number is found in the sequence. 
                 longest += 1
         longest_streak = max(longest_streak, longest)
     return longest_streak
