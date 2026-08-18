0ade918ae5d7bb548c589ef650f5b5959f4291c0
Nithish <nithish221b@gmail.com>
2026-08-18 21:46:42 +0530

    Encode_Decode.py

--- Arrays/Encode_Decode.py
+++ Arrays/Encode_Decode.py
@@ -1,13 +1,15 @@
+# Encode from text to ASCII
 def encode(input_string : str):
     encoded_string = ""
     for character in input_string : 
-        encoded_string += str(ord(character)) + " "
+        encoded_string += str(ord(character)) + " " # Adding a space to separate each character's ASCII expression
     return encoded_string.strip()
 
+# Decode from ASCII to text 
 def decode(encoded_string : str):
     decoded_string = ""
     for code in encoded_string.split():
-        decoded_string += chr(int(code))
+        decoded_string += chr(int(code))  # Convert the ASCII code back to char using char() function
     return decoded_string
 
 
