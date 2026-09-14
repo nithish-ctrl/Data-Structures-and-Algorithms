8e0bd3d2bebe532f3e3c0b287faec306d005d7d6
Nithish <nithish221b@gmail.com>
2026-09-14 19:23:27 +0530

    Three Sum

---
+++ Revision/Two Pointers/3Sum.py
@@ -0,0 +1,35 @@
+'''
+Find all unique triplets in the array that sum to zero.
+'''
+
+def threeSum(nums: list[int]) -> list[list[int]]:
+    nums.sort()
+    res = []
+    n = len(nums)
+
+    for i in range(n):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        if nums[i] > 0:
+            break
+        l, r = i + 1, n - 1
+        while l < r:
+            total = nums[i] + nums[l] + nums[r]
+            if total < 0:
+                l += 1
+            elif total > 0:
+                r -= 1
+            else:
+                res.append([nums[i], nums[l], nums[r]])
+                l += 1
+                r -= 1
+                while l < r and nums[l] == nums[l - 1]:
+                    l += 1
+                while l < r and nums[r] == nums[r + 1]:
+                    r -= 1
+    return res
+
+
+
+nums = [-1,0,1,2,-1,-4]
+print(threeSum(nums=nums))
